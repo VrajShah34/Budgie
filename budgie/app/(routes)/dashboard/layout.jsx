5c7646c964ddc57a11c16d40676b6d862642c91d
Vraj Shah <vrajsshah34@gmail.com>
2024-05-29 12:44:58 +0530

    Created Budget Page

--- budgie/app/(routes)/dashboard/layout.jsx
+++ budgie/app/(routes)/dashboard/layout.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import SideNav from './_components/SideNav' 
 import Dashboard from './page'
 import DashboardHeader from './_components/DashboardHeader'
+import { db } from '../../../utils/dbConfig'
+import { Budgets } from '../../../utils/schema'
+import { useUser } from '@clerk/nextjs'
+import { check } from 'drizzle-orm/mysql-core'
+import { eq } from 'drizzle-orm'
+import { useRouter } from 'next/navigation'
 
 function DashboardLayout({children}) {
+
+  const user = useUser();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    user&&checkUserBudgets();
+  } , [user])
+
+  const checkUserBudgets = async () => {
+    const result = await db.select()
+    .from(Budgets)
+    .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress));
+
+    console.log(result);
+
+    if(result?.length == 0){
+        router.push('/dashboard/budgets');
+    }
+  }
+
   return (
     <div>
 
